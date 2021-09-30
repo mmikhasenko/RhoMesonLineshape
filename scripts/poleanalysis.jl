94a25e7c076bcebef3e8734f44eff30d4889628c
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-09-30 18:31:20 +0200

    add variable for #bootstrap

--- scripts/poleanalysis.jl
+++ scripts/poleanalysis.jl
@@ -12,13 +12,15 @@ a0 = constwidthappr(mρ_pdg.val, Γρ_pdg.val)
 a1 = depwidthapprox(mρ_pdg.val, Γρ_pdg.val)
 
 # analysis the pole
+const Nbootstrap = 1000
+
 constwidth_pole_sample = [pole_search(constwidthappr(
 		mρ_pdg.val+randn()*mρ_pdg.err,
-		Γρ_pdg.val+randn()*Γρ_pdg.err)) for _ in 1:1000]
+		Γρ_pdg.val+randn()*Γρ_pdg.err)) for _ in 1:Nbootstrap]
 
 depwidth_pole_sample = [pole_search(depwidthapprox(
 		mρ_pdg.val+randn()*mρ_pdg.err,
-		Γρ_pdg.val+randn()*Γρ_pdg.err)) for _ in 1:1000]
+		Γρ_pdg.val+randn()*Γρ_pdg.err)) for _ in 1:Nbootstrap]
 # 
 mean_cov1 = mean_cov(constwidth_pole_sample)
 mean_cov2 = mean_cov(depwidth_pole_sample)
